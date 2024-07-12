ce96d1f0f53b77f48bd184d29500a33be42e43dd
oondels <hendriusfelix@gmail.com>
2024-07-12 16:18:38 -0300

    mostrando dados do db

--- client/src/view/ManutencaoView.vue
+++ client/src/view/ManutencaoView.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <h1>Máquinas</h1>
-    <lista-component :maquinas="maquinasObject" />
+    <manual-maquinas :maquinas="maquinasObject" />
   </div>
 </template>
 
 <script>
-import ListaComponent from "../components/ListaComponent.vue";
 import axios from "axios";
+import ManualMaquinas from "../components/ManualMaquinas.vue";
 
 export default {
   name: "manutencao-view",
   components: {
-    ListaComponent,
+    ManualMaquinas,
   },
   data() {
     return {
@@ -48,4 +48,24 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.toggle-button {
+  cursor: pointer;
+  color: #333;
+  font-weight: 500;
+  font-size: 20px;
+  margin-bottom: 15px;
+}
+
+.toggle-button:hover {
+  color: #1f75fe;
+}
+
+.toggle-button.active {
+  color: #1f75fe;
+}
+
+.hidden {
+  display: none;
+}
+</style>
